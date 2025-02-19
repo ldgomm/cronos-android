b9bc20345625d06c31fc0d1818d72ac3d1c58f96
Jose Ruiz <ruiz211@protonmail.com>
2025-02-18 22:42:39 -0500

    Cronos update

--- app/src/main/java/com/premierdarkcoffee/sales/cronos/feature/product/domain/serviceable/ProductServiceable.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/cronos/feature/product/domain/serviceable/ProductServiceable.kt
@@ -3,15 +3,12 @@ package com.premierdarkcoffee.sales.cronos.feature.product.domain.serviceable
 import com.premierdarkcoffee.sales.cronos.feature.product.data.remote.dto.ProductDto
 import com.premierdarkcoffee.sales.cronos.feature.product.domain.model.product.request.PostProductRequest
 import com.premierdarkcoffee.sales.sales.feature.product.domain.model.product.request.PutProductRequest
-import com.premierdarkcoffee.sales.cronos.util.constant.Categories
 import kotlinx.coroutines.flow.Flow
 import kotlinx.serialization.Serializable
 
 interface ProductServiceable {
 
     fun getProducts(endpoint: String, apiKey: String): Flow<Result<List<ProductDto>>>
-
-    fun getCategories(endpoint: String): Flow<Result<Categories>>
 
     fun addProduct(url: String, request: PostProductRequest, apiKey: String): Flow<Result<MessageResponse>>
 
