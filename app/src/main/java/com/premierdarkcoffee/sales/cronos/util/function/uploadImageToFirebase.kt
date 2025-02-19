0dcd32e2eca7fc9d55ca149c34f53731e0efbf3c
Jose Ruiz <ruiz211@protonmail.com>
2025-02-18 22:42:45 -0500

    Cronos update

--- app/src/main/java/com/premierdarkcoffee/sales/cronos/util/function/uploadImageToFirebase.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/cronos/util/function/uploadImageToFirebase.kt
@@ -5,20 +5,18 @@ import android.content.ContentValues.TAG
 import android.content.Context
 import android.net.Uri
 import android.util.Log
-import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.FirebaseStorage.getInstance
 import java.util.UUID
 
-fun uploadImageToFirebase(
-    context: Context,
-    contentResolver: ContentResolver,
-    uri: Uri,
-    path: String,
-    imageInfo: (ImageInfo) -> Unit
-) {
-    val remoteImagePath = "products/$path/${UUID.randomUUID()}.jpg"
+fun uploadImageToFirebase(context: Context,
+                          contentResolver: ContentResolver,
+                          uri: Uri,
+                          path: String,
+                          imageInfo: (ImageInfo) -> Unit) {
+    val remoteImagePath = "xxx/$path/${UUID.randomUUID()}.jpg"
     compressImage(uri, contentResolver, context)?.let { newUri ->
-        FirebaseStorage.getInstance().reference.child(remoteImagePath).putFile(newUri).addOnSuccessListener {
-            FirebaseStorage.getInstance().reference.child(remoteImagePath).downloadUrl.addOnSuccessListener { downloadUrl ->
+        getInstance().reference.child(remoteImagePath).putFile(newUri).addOnSuccessListener {
+            getInstance().reference.child(remoteImagePath).downloadUrl.addOnSuccessListener { downloadUrl ->
                 val info = ImageInfo(remoteImagePath, downloadUrl.toString())
                 Log.d(TAG, "uploadImageToFirebase | Success: $info")
                 imageInfo(info)
